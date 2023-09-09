9e6044a40d99fe4dcfae4dca613986784917cbe1
Veronica <almeida.vrc@gmail.com>
2023-09-09 20:38:30 -0300

    Adiciona estilização ao card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  template: `
+    <h1>Aula de Services</h1>
+    <hr />
+    <app-card></app-card>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'service';
